761a74d0069c812bf2dd950712e29d7ff49f5e37
root <root@vultr.guest>
2023-05-28 20:57:33 +0000

    Adjusted paremeter to a constant

--- fuzz/fuzz_targets/bytes_exterior.rs
+++ fuzz/fuzz_targets/bytes_exterior.rs
@@ -5,9 +5,9 @@ use libfuzzer_sys::fuzz_target;
 use bytes::{BytesMut, BufMut};
 
 
-fuzz_target!(|input: &[u16]| {
+fuzz_target!(|value: u16)| {
 
     let mut buf = BytesMut::with_capacity(1024);
     buf.put(&b"hello world"[..]);
-    buf.put_u16(input);
+    buf.put_u16(value);
 });
