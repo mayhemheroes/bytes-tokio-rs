a3e0c08f14ca0f4c772757a131b29ca7f885ced7
root <root@vultr.guest>
2023-06-01 16:42:22 +0000

    Trying to make ten test cases through simple loop

--- fuzz/fuzz_targets/bytes_exterior.rs
+++ fuzz/fuzz_targets/bytes_exterior.rs
@@ -4,11 +4,19 @@ use libfuzzer_sys::fuzz_target;
 
 use bytes::{BytesMut, BufMut};
 
+const N: usize = 10;
 
 fuzz_target!(|input: u16| {
 
+
     let mut buf = BytesMut::with_capacity(1024);
     buf.put(&b"hello world"[..]);
-    buf.put_u16(input);
+
+    for i in 0..N - 1 {
+
+        buf.put_u16(input);
+    }
+
+
 
 });
