ba2d60381e07d884aedff27bf090e32001a6de1e
root <root@vultr.guest>
2023-06-01 16:58:56 +0000

    Added a capacity fuzz var

--- fuzz/fuzz_targets/bytes_exterior.rs
+++ fuzz/fuzz_targets/bytes_exterior.rs
@@ -6,16 +6,21 @@ use bytes::{BytesMut, BufMut};
 
 const N: usize = 10;
 
-fuzz_target!(|input: u16| {
+fuzz_target!(|data: [u16, i32]| {
 
+    let (input, capacity) = data;
+    let mut buf = BytesMut::with_capacity(capacity);
 
-    let mut buf = BytesMut::with_capacity(1024);
     buf.put(&b"hello world"[..]);
 
     for i in 0..N - 1 {
 
         buf.put_u16(input);
     }
+
+    let a = buf.split();
+    // assert_eq!(a, b"hello world\x04\xD2"[..]);
+
 
 
 
